c7ed9a3bb9aff01d6005d2aa82354672c05aeed0
Eduardo Esteban Alvarez Castañeda <eduardo.a1993@gmail.com>
2021-11-30 00:37:54 -0300

    init: Crear nuevo MFE

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  devServer: {
+    static: "./",
+    port: 3003,
+  },
+  output: {
+    publicPath: "http://localhost:3003/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "auth",
+      library: { type: "var", name: "auth" },
+      filename: "@d-Una",
+      exposes: {
+        "./": "./src/App",
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
